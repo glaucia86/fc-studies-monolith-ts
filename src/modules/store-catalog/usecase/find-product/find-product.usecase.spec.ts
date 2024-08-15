34e18b25591a1f6ac9245c346478fba7b9fc6ad0
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-08-15 20:50:31 -0300

    refactor: Update FindProductUseCase and its test file
    - Update FindProductUseCase to inject the productRepository dependency
    - Update the test file for FindProductUseCase to use the injected productRepository

--- src/modules/store-catalog/usecase/find-product/find-product.usecase.spec.ts
+++ src/modules/store-catalog/usecase/find-product/find-product.usecase.spec.ts
@@ -7,7 +7,7 @@
 
 import Id from "../../../@shared/domain/value-object/id.value-object";
 import Product from "../../domain/product.entity";
-import FindAllProductsUseCase from "../find-all-products/find-all-products.usecase";
+import FindProductUseCase from "./find-product.usecase";
 
 const product = new Product({
   id: new Id("1"),
@@ -26,7 +26,7 @@ const MockProductRepository = () => {
 describe("Find Product UseCase Unit Test", () => {
   it("should get product by id", async () => {
     const productRepository = MockProductRepository();
-    const useCase = new FindProductUseCase();
+    const useCase = new FindProductUseCase(productRepository);
 
     const input = {
       id: '1',
