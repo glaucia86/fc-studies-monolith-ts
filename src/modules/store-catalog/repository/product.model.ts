2ac768cf1c67b95a4c1c3272428183f76f316b2c
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-08-15 19:46:41 -0300

    feat: Add `ProductModel` for store-catalog
    This commit adds the `ProductModel` class to the store-catalog repository. The `ProductModel` represents the definition of a product, including properties such as id, name, description, and salesPrice.

---
+++ src/modules/store-catalog/repository/product.model.ts
@@ -0,0 +1,28 @@
+/**
+ * file: src/modules/store-catalog/repository/product.model.ts
+ * description: file responsible for the definition of the product model.
+ * data: 08/15/2024
+ * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
+ */
+
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+  tableName: "products",
+  timestamps: false,
+})
+export default class ProductModel extends Model {
+
+  @PrimaryKey
+  @Column({ allowNull: false })
+  declare id: string;
+
+  @Column({ allowNull: false })
+  declare name: string;
+
+  @Column({ allowNull: false })
+  declare description: string;
+
+  @Column({ allowNull: false })
+  declare salesPrice: number;
+}
