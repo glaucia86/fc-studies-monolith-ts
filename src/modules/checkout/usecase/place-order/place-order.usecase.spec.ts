ae684e1cb36eb04d8a3d316f04d631cae52c1f39
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-12-15 22:49:49 -0300

    feat: Add unit test for handling invalid products in `PlaceOrder` use case

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -27,6 +27,33 @@ describe('PlaceOrder UseCase unit Test', () => {
       }
 
       await expect(placeOrderUseCase.execute(input)).rejects.toThrow(new Error('Client not found'));
-    })
-  })
-})
+    });
+
+    it('should throw an error when product are not valid', async () => {
+      const mockClientFacade = {
+        find: jest.fn().mockResolvedValue(null),
+      };
+
+      //@ts-expect-error - no params in constructor
+      const placeOrderUseCase = new PlaceOrderUseCase();
+
+      const mockValidateProducts = jest
+        //@ts-expect-error - spy on private method
+        .spyOn(placeOrderUseCase, 'validateProducts')
+        //@ts-expect-error - not return never
+        .mockRejectedValue(new Error('No products selected'));
+
+      //@ts-expect-error - force set clientFacade
+      placeOrderUseCase['_clientFacade'] = mockClientFacade;
+
+      const input: PlaceOrderInputDto = {
+        clientId: '1',
+        products: [],
+      }
+
+      await expect(placeOrderUseCase.execute(input)).rejects.toThrow(new Error('No products selected'));
+
+      expect(mockValidateProducts).toHaveBeenCalledTimes(1);
+    });
+  });
+});
