08664b7ca263e3f4dbfbf59cedf255e37f3bb143
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2024-12-16 22:29:12 -0300

    feat: Add unit tests for getProduct and execute methods in PlaceOrder use case

--- src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
+++ src/modules/checkout/usecase/place-order/place-order.usecase.spec.ts
@@ -5,6 +5,8 @@
  * author: Glaucia Lemos <Twitter: @glaucia_lemos86>
  */
 
+import Id from "../../../@shared/domain/value-object/id.value-object";
+import Product from "../../domain/product.entity";
 import { PlaceOrderInputDto } from "./place-order.dto";
 import PlaceOrderUseCase from "./place-order.usecase";
 
@@ -75,54 +77,79 @@ describe('PlaceOrder UseCase unit Test', () => {
       placeOrderUseCase['_catalogFacade'] = mockCatalogFacade;
 
       await expect(placeOrderUseCase['getProduct']('0')).rejects.toThrow(new Error('Product not found'));
-    })
-  });
+    });
 
-  describe('execute method', () => {
-    // buscar o cliente. Caso não encontre -> client not found
-    it('should throw an error when client not found', async () => {
-      const mockClientFacade = {
-        find: jest.fn().mockResolvedValue(null),
+    it('should return a product', async () => {
+      const mockCatalogFacade = {
+        findById: jest.fn().mockResolvedValue({
+          id: '0',
+          name: 'Product 0',
+          description: 'Product 0 description',
+          salesPrice: 0,
+        }),
       };
 
       //@ts-expect-error - no params in constructor
-      const placeOrderUseCase = new PlaceOrderUseCase();
-      //@ts-expect-error - force set clientFacade
-      placeOrderUseCase['_clientFacade'] = mockClientFacade;
+      placeOrderUseCase['_catalogFacade'] = mockCatalogFacade;
 
-      const input: PlaceOrderInputDto = {
-        clientId: '0',
-        products: [],
-      }
+      await expect(placeOrderUseCase['getProduct']('0')).resolves.toEqual(
+        new Product({
+          id: new Id('0'),
+          name: 'Product 0',
+          description: 'Product 0 description',
+          salesPrice: 0,
+        })
+      );
 
-      await expect(placeOrderUseCase.execute(input)).rejects.toThrow(new Error('Client not found'));
-    });
-
-    it('should throw an error when product are not valid', async () => {
-      const mockClientFacade = {
-        find: jest.fn().mockResolvedValue(true),
-      };
-
-      //@ts-expect-error - no params in constructor
-      const placeOrderUseCase = new PlaceOrderUseCase();
-
-      const mockValidateProducts = jest
-        //@ts-expect-error - spy on private method
-        .spyOn(placeOrderUseCase, 'validateProducts')
-        //@ts-expect-error - not return never
-        .mockRejectedValue(new Error('No products selected'));
-
-      //@ts-expect-error - force set clientFacade
-      placeOrderUseCase['_clientFacade'] = mockClientFacade;
-
-      const input: PlaceOrderInputDto = {
-        clientId: '1',
-        products: [],
-      }
-
-      await expect(placeOrderUseCase.execute(input)).rejects.toThrow(new Error('No products selected'));
-
-      expect(mockValidateProducts).toHaveBeenCalledTimes(1);
+      expect(mockCatalogFacade.find).toHaveBeenCalledTimes(1);
     });
   });
+
+describe('execute method', () => {
+  // buscar o cliente. Caso não encontre -> client not found
+  it('should throw an error when client not found', async () => {
+    const mockClientFacade = {
+      find: jest.fn().mockResolvedValue(null),
+    };
+
+    //@ts-expect-error - no params in constructor
+    const placeOrderUseCase = new PlaceOrderUseCase();
+    //@ts-expect-error - force set clientFacade
+    placeOrderUseCase['_clientFacade'] = mockClientFacade;
+
+    const input: PlaceOrderInputDto = {
+      clientId: '0',
+      products: [],
+    }
+
+    await expect(placeOrderUseCase.execute(input)).rejects.toThrow(new Error('Client not found'));
+  });
+
+  it('should throw an error when product are not valid', async () => {
+    const mockClientFacade = {
+      find: jest.fn().mockResolvedValue(true),
+    };
+
+    //@ts-expect-error - no params in constructor
+    const placeOrderUseCase = new PlaceOrderUseCase();
+
+    const mockValidateProducts = jest
+      //@ts-expect-error - spy on private method
+      .spyOn(placeOrderUseCase, 'validateProducts')
+      //@ts-expect-error - not return never
+      .mockRejectedValue(new Error('No products selected'));
+
+    //@ts-expect-error - force set clientFacade
+    placeOrderUseCase['_clientFacade'] = mockClientFacade;
+
+    const input: PlaceOrderInputDto = {
+      clientId: '1',
+      products: [],
+    }
+
+    await expect(placeOrderUseCase.execute(input)).rejects.toThrow(new Error('No products selected'));
+
+    expect(mockValidateProducts).toHaveBeenCalledTimes(1);
+  });
+});
 });
